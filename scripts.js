6fd6ea96c21830a364a768f0b2f45d972bbe9e16
vyangit <vyan.git@gmail.com>
2017-05-11 17:28:31 -0700

    Done initial implementation

--- scripts.js
+++ scripts.js
@@ -1,10 +1,31 @@
 $(document).ready(function(){
+
+  var toShow = "";
+
   $("span.menu-icon").click(function(){
-    $("div.bar1, div.bar2, div.bar3").toggleClass("expand");
+    $("div").toggleClass("expand");
+    $("div#menu-items").fadeToggle('2000');
+  });
 
-    $("div#menu-items").show(function(){
-      
-    })
-  })
+  $("li.menu-btn").mouseenter(function(){
+    $(this).animate({fontSize:'23px'}, 'fast');
+  });
 
+  $("li.menu-btn").mouseleave(function(){
+    $(this).animate({fontSize:'20px'}, 'fast');
+  });
+
+  $("li.menu-btn").click(function(){
+    if ($("#intro").css("display") === "block"){
+      $("#intro").hide();
+      toShow = "#" + $(this).text();
+      $("#content").fadeIn('slow');
+      $(toShow).show();
+    } else {
+      var toHide = toShow;
+      toShow = "#" + $(this).text();
+      $(toHide).hide();
+      $(toShow).fadeIn('slow');
+    };
+  });
 });

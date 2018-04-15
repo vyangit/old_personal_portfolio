8b0d33fffb1adf134e4407cf7129375cb3dd7fe2
Victor Yan <vyan.git@gmail.com>
2018-04-14 21:02:33 -0700

    1) Starting new implementation

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ import ProjectSection from './js/project';
 import Footer from './js/footer';
 
 ReactDOM.render(<Header />, document.getElementById('header'));
-ReactDOM.render(<EducationSection />, document.getElementById('work'));
-ReactDOM.render(<WorkSection />, document.getElementById('projects'));
+ReactDOM.render(<EducationSection />, document.getElementById('education'));
+ReactDOM.render(<WorkSection />, document.getElementById('work'));
 ReactDOM.render(<ProjectSection />, document.getElementById('projects'));
 ReactDOM.render(<Footer />, document.getElementById('footer'));
